be00cc4eac07ab71560c4bc645c15d91c5e254dc
bruno kiprop <brunookiprop@gmail.com>
2021-03-06 21:54:29 +0300

    completed about me

--- src/index.js
+++ src/index.js
@@ -41,10 +41,89 @@ class Landing extends React.Component{
                         <button className={"btn btn-primary"}>MY PROJECTS</button>
                     </div>
                 </div>
-                <div className={"my-image"} alt="Bruno Image">
+                <div></div>
+            </div>
+        );
+    }
+}
+
+class AboutMe extends React.Component{
+    render(){
+        return(
+            <div id={"about-me-container"}>
+                <div id={"about-me-inner"}>
+                    <h2 className={"text-primary"}>About Me</h2>
+                    <div id={"about-me"}>
+                        <LanguagesAndFrameworks />
+                        <SkillsAndApplications />
+                        <QualitiesAndMerits/>
+                    </div>
                 </div>
             </div>
         );
+    }
+}
+
+class LanguagesAndFrameworks extends React.Component{
+    render(){
+        return(
+            <div className={"section"}>
+                <h3 className={"title"}>Languages and Frameworks</h3>
+                <ul>
+                    <li>Kotlin</li>
+                    <li>Java</li>
+                    <li>React</li>
+                    <li>JavaScript and JQuery</li>
+                    <li>PHP</li>
+                    <li>Bootstrap</li>
+                    <li>HTML and CSS</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+class SkillsAndApplications extends React.Component{
+    render(){
+        return(
+            <div className={"section"}>
+                <h3 className={"title"}>Skills and Applications</h3>
+                <ul>
+                    <li>Android Development</li>
+                    <li>Web Development</li>
+                    <li>UI & UX design</li>
+                    <li>Wireframing</li>
+                    <li>Prototyping</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+class QualitiesAndMerits extends React.Component{
+    render(){
+        return(
+            <div className={"section"}>
+                <h3 className={"title"}>Qualities and Merits</h3>
+                <ul>
+                    <li>Writing clean & testable code</li>
+                    <li>Timely Delivery</li>
+                    <li>Team work and leadership</li>
+                    <li>Effective Communication</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+class Background extends React.Component {
+    render(){
+        return(
+            <div id={"background"} className={"full-screen"}>
+                <div></div>
+                <div className={"my-image"}></div>
+            </div>
+        )
     }
 }
 
@@ -52,8 +131,10 @@ class Body extends React.Component{
     render(){
         return(
             <div>
+                <Background />
                 <Landing />
                 <Socials />
+                <AboutMe />
             </div>
         )
     }
